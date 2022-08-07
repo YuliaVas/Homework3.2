0cecd0ae70b6d9a18cd484479b51108c4551b5f0
YuliyaVas <yulenka2596@gmail.com>
2022-08-08 02:05:54 +0300

    Добавьте файлы проекта.

---
+++ Homework3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { while(true)
+                {
+                Console.WriteLine("Введите 2 числа");
+                int A = Convert.ToInt32(Console.ReadLine());
+                int B = Convert.ToInt32(Console.ReadLine());
+                if (A > B)
+                { Console.WriteLine(A + " больше " + B);
+                }
+                if (B > A) 
+                { Console.WriteLine(A + " меньше " + B);
+                }
+                if (A == B) 
+                { Console.WriteLine(A + " равно " + B);
+                } }
+
+        }
+    }
+}
